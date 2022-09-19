1b1005f1ca347e7cbb2757a171e23ee94e9817ef
RohitPrajapati9593 <rohitprajapati.bbit@gmail.com>
2022-09-19 10:12:27 +0530

    Second Commit for Lab-9:example 2

---
+++ lib/lab_9_2/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:lab_9_1/lab_9_2/choose_location.dart';
+import 'package:lab_9_1/lab_9_2/home.dart';
+import 'package:lab_9_1/lab_9_2/loading.dart';
+
+void main() => runApp(MaterialApp(initialRoute: '/home', routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/location': (context) => ChooseLocation(),
+    }));
