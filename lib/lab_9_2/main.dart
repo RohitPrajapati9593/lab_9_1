6a9c849e64dbeb7110009ada14fe55473522773e
RohitPrajapati9593 <rohitprajapati.bbit@gmail.com>
2022-09-19 21:34:35 +0530

    Second Commit for Lab-9:example 2

--- lib/lab_9_2/main.dart
+++ lib/lab_9_2/main.dart
@@ -3,7 +3,9 @@ import 'package:lab_9_1/lab_9_2/choose_location.dart';
 import 'package:lab_9_1/lab_9_2/home.dart';
 import 'package:lab_9_1/lab_9_2/loading.dart';
 
-void main() => runApp(MaterialApp(initialRoute: '/home', routes: {
+void main() => runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    initialRoute: '/home', routes: {
       '/': (context) => Loading(),
       '/home': (context) => Home(),
       '/location': (context) => ChooseLocation(),
